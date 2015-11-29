86bd0148006bb7d759dbd512a748866d37cb4b93
Charles Whitfield <whitfield.cw@gmail.com>
2015-11-28 22:50:30 -0800

    add editline functions to allow arrow navigation of prompt input

--- prompt.c
+++ prompt.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+#include <editline/readline.h>
 /*declare buffer for user input of size 2048*/
-
-static char input[2048];
 
 int main(int argc, char** argv){
 
@@ -12,17 +12,19 @@ int main(int argc, char** argv){
 
 	/*never-ending loop*/
 	while (1){
-		
-		/*output prompt*/
-		fputs("lisp clone> ", stdout);
+
+		/*output prompt and get input*/
+		char* input = readline("lisp clone> ");
 
 		/* read a line of user input max size 2048*/
-		fgets(input, 2048, stdin);
+		add_history(input);
 
 		/*echo in put back to user*/
-		printf("no you're a %s", input);
+		printf("no you're a %s\n", input);
+
+		/*free retrieved input*/
+		free(input);
 	}
 
 	return 0;
 }
-

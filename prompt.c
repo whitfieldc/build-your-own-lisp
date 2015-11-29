508b2cb28235e0f1c98e68a32c3bcc60f65f33eb
Charles Whitfield <whitfield.cw@gmail.com>
2015-11-29 11:36:07 -0800

    add windows compatibility

--- prompt.c
+++ prompt.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer [2048];
+
+/* fake readline function*/
+char* readline(char* prompt){
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer)+1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy)-1] = '\0';
+	return cpy
+}
+
+/* fake add_history function*/
+void add_history(char* unused){}
+
+/*otherwise include editline */
+#else
 #include <editline/readline.h>
-/*declare buffer for user input of size 2048*/
+#endif
 
 int main(int argc, char** argv){
 

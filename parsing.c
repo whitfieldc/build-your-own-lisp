f375657ec1c68072fffa0dd9776ee4bf4c762e68
Charles <whitfield.cw@gmail.com>
2015-12-01 23:55:13 -0800

    add basic arithmetic REPL

--- parsing.c
+++ parsing.c
@@ -25,6 +25,40 @@ void add_history(char* unused){}
 #include <editline/readline.h>
 #endif
 
+/* use operator to see which operation to perform */
+long eval_op(long x, char* op, long y){
+	if (strcmp(op, "+") == 0) { return x + y; }
+	if (strcmp(op, "-") == 0) { return x - y; }
+	if (strcmp(op, "*") == 0) { return x * y; }
+	if (strcmp(op, "/") == 0) { return x / y; }
+	return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+	/* if tagged as number return directly */
+	if (strstr(t->tag, "number")){
+		return atoi(t->contents);
+	}
+
+	/* operator is always second child*/
+	char* op = t->children[1]->contents;
+
+	/* we store 3rd child in 'x' */
+	long x = eval(t->children[2]);
+
+	/* iterate remaining children and combine */
+	int i = 3;
+	while (strstr(t->children[i]->tag, "expr")){
+		x = eval_op(x, op, eval(t->children[i]));
+		i++;
+	}
+
+	return x;
+
+}
+
+
 int main(int argc, char** argv){
 
 	/* create some parsers */
@@ -60,7 +94,10 @@ int main(int argc, char** argv){
 		mpc_result_t r;
 		if (mpc_parse("<stdin>", input, Lispy, &r)) {
 		  /* On Success Print the AST */
-		  mpc_ast_print(r.output);
+		  // mpc_ast_print(r.output);
+		  long result = eval(r.output);
+			printf("%li\n", result);
+			mpc_ast_delete(r.output);
 		  mpc_ast_delete(r.output);
 		} else {
 		  /* Otherwise Print the Error */
